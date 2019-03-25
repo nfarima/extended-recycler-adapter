2129d121f5fc7c2fe0215b969b454efb4a4032a3
Nicolae FARIMA <nfarima@pentalog.fr>
2019-03-25 17:20:29 +0200

    renamed to Extended Recycler Adapter

--- app/src/main/java/com/github/nfarima/extendedrecycleradapterexample/MainActivity.java
+++ app/src/main/java/com/github/nfarima/extendedrecycleradapterexample/MainActivity.java
@@ -3,7 +3,6 @@ package com.github.nfarima.extendedrecycleradapterexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.github.nfarima.extendedrecycleradapter.extendedrecycleradapter;
 
 public class MainActivity extends AppCompatActivity {
 
