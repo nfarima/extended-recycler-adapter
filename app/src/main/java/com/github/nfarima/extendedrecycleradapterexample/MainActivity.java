507f95c06dce5c3facceacc3496b1821c16cb52b
Nicolae FARIMA <nfarima@pentalog.fr>
2019-03-26 08:21:52 +0200

    changed minimum API level

--- app/src/main/java/com/github/nfarima/extendedrecycleradapterexample/MainActivity.java
+++ app/src/main/java/com/github/nfarima/extendedrecycleradapterexample/MainActivity.java
@@ -2,6 +2,14 @@ package com.github.nfarima.extendedrecycleradapterexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.github.nfarima.extendedrecycleradapter.ExtendedRecyclerAdapter;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> names = Arrays.asList("Black Widow", "Hulk", "Thanos", "Maw", "Dr. Strange", "Dormamu");
 
-    }
+        RecyclerView recycler = findViewById(R.id.recycler);
+        new ExtendedRecyclerAdapter<TextView, String>(recycler)
+                .viewFactory(() -> new TextView(this))
+                .data(() -> names)
+                .bind(TextView::setText)
+                .vertical();    }
 }
